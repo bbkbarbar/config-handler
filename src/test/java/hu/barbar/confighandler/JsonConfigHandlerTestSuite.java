2de13273845d0a6c242ef13432debfc4989405a4
Barbár <bbk.barbar@gmail.com>
2017-07-04 09:53:39 +0200

    Add test case

--- src/test/java/hu/barbar/confighandler/JsonConfigHandlerTestSuite.java
+++ src/test/java/hu/barbar/confighandler/JsonConfigHandlerTestSuite.java
@@ -1,9 +1,6 @@
 package hu.barbar.confighandler;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,14 +8,29 @@ import org.junit.Test;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for JsonConfigHandler class.
  */
 public class JsonConfigHandlerTestSuite extends TestSuite {
 	
+	private static final String CONFIG_JSON = "sample_config.json";
+	
+	private static JsonConfigHandler config = null;
+	
+	
+	@Before
+	public void before() {
+		config = new JsonConfigHandler(CONFIG_JSON);
+	}
+	
 	
 	@Test
-	public void firstTest() {
-		assertTrue(true);
+	public void readSingleKeyFromJsonTest() {
+        assertEquals("value1", config.getString("parameter1"));
+	}
+	
+	@Test
+	public void readNestedKeyFromJsonTest() {
+        assertEquals("sub-value1", config.getString("parameter_root.sub-parameter1"));
 	}
 	
 
